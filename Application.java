aca0e672c3831416b8ad157f709a87ca6deb62ae
pranaymiddleware <pranaymiddleware@gmail.com>
2023-02-11 23:56:29 +0530

    feature202 changes been added by harsha

--- Application.java
+++ Application.java
@@ -18,3 +18,21 @@ public class Main {
       System.out.println(num + " is not a prime number.");
   }
 }
+
+===================================================
+Feature 202 changes has been added by harsha on forloop module on jan 2023
+
+// print array elements 
+
+class Main {
+  public static void main(String[] args) {
+      
+    // create an array
+    int[] numbers = {3, 9, 5, -5};
+    
+    // for each loop 
+    for (int number: numbers) {
+      System.out.println(number);
+    }
+  }
+}
